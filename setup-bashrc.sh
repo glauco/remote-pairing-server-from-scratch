c83417a102b979f97ed76321ebab32f92402c62c
Glauco Vinicius <gl4uc0@gmail.com>
2017-04-11 18:27:47 -0300

    Updates setup procedure

--- setup-bashrc.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo 'export PATH=$HOME/usr/local/bin:$PATH' >> ~/.bashrc
-source ~/.bashrc
---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo 'export PATH=$HOME/usr/local/bin:$PATH' >> "$HOME/.bashrc"
+source "$HOME/.bashrc"
+
+echo 'Installing libevent…'
+bash install-libevent.sh
+
+echo 'Installing ncurses…'
+bash install-ncurses.sh
+
+echo 'Installing tmux…'
+bash install-tmux.sh
+
+echo 'Installing vim…'
+bash install-vim.sh
+
+echo 'Installing ack…'
+bash install-ack.sh
