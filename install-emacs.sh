c7038dcfe17b5bd095197290924f3422ba99a074
Glauco Vinicius <gl4uc0@gmail.com>
2017-04-11 18:41:35 -0300

    Adds Emacs installation script

---
+++ install-emacs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir -p "$HOME/usr/local"
+wget --no-check-certificate ftp://ftp.gnu.org/pub/gnu/emacs/emacs-25.1.tar.gz -O emacs.tar.gz
+tar -xvzf emacs.tar.gz
+pushd emacs-25.1
+./configure --prefix="$HOME/usr/local" --with-x-toolkit=no --with-gif=no --with-tiff=no
+make
+make install
+popd
+rm emacs.tar.gz
+rm -rf emacs-25.1
