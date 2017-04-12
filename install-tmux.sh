de39ad2fa4b65df5db9ce927562417e0b0f32243
Glauco Vinicius <gl4uc0@gmail.com>
2017-04-12 10:42:15 -0300

    Adds prefix parameter

--- install-tmux.sh
+++ install-tmux.sh
@@ -4,7 +4,7 @@ mkdir -p "$HOME/usr/local"
 wget --no-check-certificate https://github.com/tmux/tmux/releases/download/2.3/tmux-2.3.tar.gz -O tmux.tar.gz
 tar -xvzf tmux.tar.gz
 pushd tmux-2.3
-./configure CFLAGS="-I$HOME/usr/local/include -I$HOME/usr/local/include/ncurses" LDFLAGS="-L$HOME/usr/local/lib -L$HOME/usr/local/include/ncurses -L$HOME/usr/local/include"
+./configure --prefix="$HOME/usr/local" CFLAGS="-I$HOME/usr/local/include -I$HOME/usr/local/include/ncurses" LDFLAGS="-L$HOME/usr/local/lib -L$HOME/usr/local/include/ncurses -L$HOME/usr/local/include"
 CPPFLAGS="-I$HOME/usr/local/include -I$HOME/usr/local/include/ncurses" LDFLAGS="-static -L$HOME/usr/local/include -L$HOME/usr/local/include/ncurses -L$HOME/usr/local/lib" make
 CPPFLAGS="-I$HOME/usr/local/include -I$HOME/usr/local/include/ncurses" LDFLAGS="-static -L$HOME/usr/local/include -L$HOME/usr/local/include/ncurses -L$HOME/usr/local/lib" make install
 popd
